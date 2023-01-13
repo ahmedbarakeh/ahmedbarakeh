14099827abbdd08592fea31d970959055d380ebb
AhmedBarakeh2 <albarakehahmed@gmail.com>
2023-01-13 15:43:57 +0200

    creating new var for testing

--- lib/bindings/settings_binding.dart
+++ lib/bindings/settings_binding.dart
@@ -5,6 +5,7 @@ import '../controllers/settings_controller.dart';
 class SettingsBinding implements Bindings {
   @override
   void dependencies() {
+    var x = 1;
     Get.lazyPut(() => SettingsController());
   }
 }
